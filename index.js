10b41cb85682a1381a81cabe17516bc62638223b
pentaled <89256667+pentaled@users.noreply.github.com>
2021-09-04 18:11:56 +1000

    alpha v.10.4 - commit

--- index.js
+++ index.js
@@ -7,8 +7,22 @@ require('dotenv').config()
 app.listen(3000);
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('public'))
-
+app.use(express.urlencoded({ extended: true }))
+// get use querry //
 app.get('/', async (req, res) => {
 	const results = await Task.read()
 	res.render('home', { tasks: results });
+});
+// post use body //
+app.post('/task/create', async (req, res) => {
+	if (req.body.task !== '') {
+		console.log(req.body.task)
+		await Task.create(req.body.task)
+	}
+	res.redirect('/')
+});
+
+app.get('/task/delete/:id', async (req, res) => {
+	await Task.destroy(req.params.id)
+	res.redirect('/')
 });
--- models/Task.js
+++ models/Task.js
@@ -1,15 +1,26 @@
 const mysql = require('mysql2/promise');
 
 module.exports = {
-    async read() {
+    async execute(query, params = []) {
         const connection = await mysql.createConnection({
             host: process.env.DB_HOST,
             user: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME
         })
-        
-        const [rows] =  await connection.execute('SELECT * FROM todo.tasks;') //('SELECT * FROM `table` WHERE `name` = ? AND `age` > ?', ['Morty', 14])//
+        return await connection.execute(query, params)
+    },
+    async read() {
+        const [rows] =  await this.execute('SELECT * FROM todo.tasks WHERE todo.tasks.status = (?);', ['NEW'])
+        return rows
+    },
+    async create(task) {
+        const [rows] =  await this.execute('INSERT INTO todo.tasks (todo.tasks.task) values (?);', [task])
+        return rows
+    },
+    async destroy(id) {
+        const [rows] =  await this.execute('DELETE FROM todo.tasks WHERE todo.tasks.id = (?);', [id])
         return rows
     }
 }
+
