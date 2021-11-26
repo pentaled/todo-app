bd2787196b8d9ef744cf176902aec8298d6994b7
Matthew Chong <matthewkc7@gmail.com>
2021-11-27 08:18:42 +1100

    Mock data

---
+++ src/mock-data.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+        "id": "4101",
+        "description": "Buy Milk",
+        "status": "NEW"
+    },
+    {
+        "id": "4102",
+        "description": "Plan weekend outing",
+        "status": "NEW"
+    },
+    {
+        "id": "4103",
+        "description": "Publish Friday blog post",
+        "status": "NEW"
+    },
+    {
+        "id": "4104",
+        "description": "Run 3 miles",
+        "status": "NEW"
+    },
+    {
+        "id": "4105",
+        "description": "Wash clothes",
+        "status": "NEW"
+    },
+    {
+        "id": "4106",
+        "description": "Update Database",
+        "status": "NEW"
+    }
+]
+
+export default data;
